61aea067134254a221bd607b0fa1135ce64af1e4
unknown <mgehring9@gmail.com>
2022-11-20 11:59:38 -0800

    support linux and windows clear functions

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # import only system from os 
 from os import system, name 
+import platform
   
 # import sleep to show output for some time period 
 from time import sleep
@@ -9,7 +10,7 @@ from Board import *
 def game(init):
     i = init
     if i == 0:
-        gameBoard = Board(80, 40)
+        gameBoard = Board(40, 40)
         gameBoard.newGame()
         gameBoard.seed()
         i= 1
@@ -19,4 +20,7 @@ def game(init):
         #print('generation: ' + str(i))
         #i += 1
         sleep(0)
-        system('cls')
+        if(platform.system() == "Windows"):
+            system('cls')
+        else:
+            system('clear')
